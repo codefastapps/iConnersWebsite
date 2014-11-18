e32d9ba9a4d3d329c89ed3d6d885129a052f15bd
Shawn Conners <sconners@kissyfaceinteractive.com>
2014-11-17 20:27:29 -0700

    Beginning Changes

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="iConners, the online resume and portfolio of Shawn Conners.">
+    <meta name="author" content="Shawn Conners">
+    <link rel="icon" href="favicon.ico">
+    <title>iConners - The online resume and portfolio of Shawn Conners.</title>
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-old-ie-min.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css">
+    <!--<![endif]-->
+
+    <style>
+    	
+    </style>
+    <script>
+    
+    	function funGlobalStartup(){
+    		
+    		funLocalStartup();
+    	}
+    
+    </script>
+    
+  </head>
+
+  <body onload="funGlobalStartup()">
+
+    <div class="pure-menu pure-menu-open pure-menu-horizontal">
+    	<a href="#" class="pure-menu-heading">iConners</a>
+		<ul>
+        	<li class="pure-menu-selected"><a href="#">Home</a></li>
+        	<li><a href="#">Previous Employers</a></li>
+        	<li><a href="#">Technologies</a></li>
+        	<li><a href="#">Education</a></li>
+        	<li><a href="#">Contact</a></li>
+    	</ul>
+	</div>
+
--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-index.php
+<?php include "header.php"; ?>
+	  <script>
+	  	function funLocalStartup(){
+	  		// Change Navigation Link to Active
+	  		document.getElementById("linkHome").className = "active";
+	  	}
+	  </script>
+	
+<?php include "footer.php"; ?>
